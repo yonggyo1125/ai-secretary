76629c6aea93254884aecf69f55fc013c2fc6846
yonggyo1981 <yonggyo00@kakao.com>
2026-04-05 13:50:43 +0900

    .

---
+++ src/main/java/org/sparta/secretary/AiSecretaryApplication.java
@@ -0,0 +1,13 @@
+package org.sparta.secretary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSecretaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiSecretaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sparta/secretary/AiSecretaryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.secretary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSecretaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
